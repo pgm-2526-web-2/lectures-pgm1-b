6ef9eb8b077b937744770a335ae6d1c85fb10a7e
Dieter Peirs <github@dieterpeirs.com>
2026-01-08 14:01:18 +0100

    add params demo

--- week-6/url-demo/js/main.js
+++ week-6/url-demo/js/main.js
@@ -1,1 +1,18 @@
-(() => {})();
+(() => {
+  console.log(window.location.search);
+
+  const params = new URLSearchParams(window.location.search);
+  const $name = document.getElementById("name");
+
+  console.log(params);
+
+  let name = "";
+
+  if (params.has("name")) {
+    name = params.get("name");
+  } else {
+    name = "anonieme gebruiker";
+  }
+
+  $name.textContent = name;
+})();
